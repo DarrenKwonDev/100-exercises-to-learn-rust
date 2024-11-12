36402f6df450afa1b26b81946f963d32a773acfb
drkup <darrenkwondev46@gmail.com>
2024-11-12 22:03:00 +0900

    ~9

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -11,9 +11,17 @@ mod tests {
     use super::Ticket;
     use std::mem::size_of;
 
+
+    /*
+    
+    The pointer to the heap region you reserved.
+    The length of the string, i.e. how many bytes are in the string.
+    The capacity of the string, i.e. how many bytes have been reserved on the heap.
+    |ptr|len|cap|
+     */
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        assert_eq!(size_of::<String>(), 8 * 3);
     }
 
     #[test]
@@ -23,6 +31,6 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Type layout" section of The Rust Reference
         // https://doc.rust-lang.org/reference/type-layout.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), 24 * 3);
     }
 }
